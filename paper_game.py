b92bab9ad7a30e62a4398bb4a83395c002358a60
Камиль <kamil.kamalov49@gmail.com>
2025-10-20 13:54:57 +0300

    Добавлена игра Камень, ножницы, бумага

---
+++ paper_game.py
@@ -0,0 +1,13 @@
+User_choice = input("Ваш выбор строчными буквами: ")
+
+if User_choice == "камень":
+    print("ничья")
+elif User_choice == "ножницы":
+    print("поражение")
+elif User_choice == "бумага":
+    print("победа")
+else:
+    print("Выберите между значениями 'камень' 'ножницы' 'бумага'")
+
+
+
