f4740bbb1ceefeee53d8f05facad6c8c92ecd2b1
Камиль <kamil.kamalov49@gmail.com>
2025-10-27 15:38:38 +0300

    третье домашнее задание

---
+++ homework_3.py
@@ -0,0 +1,26 @@
+# m:int = int(input())
+# n:int = int(input())
+# numbers = list(range(m, n + 1))
+# for num in numbers:
+#     if num % 17 == 0:
+#         print(num)
+#     elif num % 10 == 9:
+#         print(num)
+#     elif num % 3 == 0 and num % 5 == 0:
+#         print(num)
+
+# n:int = int(input())
+# numbers = list(range(1, n + 1))
+# for num in numbers:
+#     if 5 <= num <= 9:
+#         continue
+#     if 17 <= num <= 37:
+#         continue
+#     if 78 <= num <= 87:
+#         continue
+#     print(num)
+
+n = int(input())
+
+for i in range(n):
+    print(n, n, n)
